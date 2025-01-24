4ef3ec4504bbf1f69f01d40fb158caad01549f24
Kojo Acquah <kojoacquah7@gmail.com>
2025-01-24 13:43:47 -0400

    started integrating collections into portfolio

--- src/content.config.js
+++ src/content.config.js
@@ -6,7 +6,7 @@ import { glob, file } from 'astro/loaders';
 
 const md_and_mdx_patterns = ["**/*.mdx", "**/*.mdx"]
 // 3. Define your collection(s)
-const blog = defineCollection({ pattern: md_and_mdx_patterns, base: "./data/blog" });
+const blog = defineCollection({ pattern: md_and_mdx_patterns, base: "./data/posts/blog" });
 const portfolio = defineCollection({ pattern: md_and_mdx_patterns, base: "./data/portfolio" });
 
 // 4. Export a single `collections` object to register your collection(s)
