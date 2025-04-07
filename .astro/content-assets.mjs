2d7c26dd310536c550d84d9c7b346e87ecff9178
Kojo Acquah <kojoacquah7@gmail.com>
2025-04-06 21:21:23 -0300

    added homepage content

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,7 +1,7 @@
 
-import __ASTRO_IMAGE_IMPORT_ZA5H9G from "../portfolio/images/image_new.jpg?astroContentImageFlag=&importer=src%2Fportfolio%2Ftest_item.md";
 import __ASTRO_IMAGE_IMPORT_XvF3M from "../portfolio/images/image_new.jpg?astroContentImageFlag=&importer=src%2Fportfolio%2Ftest_item_2.md";
 import __ASTRO_IMAGE_IMPORT_Z2fuMYa from "../portfolio/images/image_new.jpg?astroContentImageFlag=&importer=src%2Fportfolio%2Fwhat_cms.md";
+import __ASTRO_IMAGE_IMPORT_ZA5H9G from "../portfolio/images/image_new.jpg?astroContentImageFlag=&importer=src%2Fportfolio%2Ftest_item.md";
 import __ASTRO_IMAGE_IMPORT_1uno4J from "../portfolio/images/image_new.jpg?astroContentImageFlag=&importer=src%2Fportfolio%2Fwhat_cms_update.md";
-export default new Map([["../portfolio/images/image_new.jpg?astroContentImageFlag=&importer=src%2Fportfolio%2Ftest_item.md", __ASTRO_IMAGE_IMPORT_ZA5H9G], ["../portfolio/images/image_new.jpg?astroContentImageFlag=&importer=src%2Fportfolio%2Ftest_item_2.md", __ASTRO_IMAGE_IMPORT_XvF3M], ["../portfolio/images/image_new.jpg?astroContentImageFlag=&importer=src%2Fportfolio%2Fwhat_cms.md", __ASTRO_IMAGE_IMPORT_Z2fuMYa], ["../portfolio/images/image_new.jpg?astroContentImageFlag=&importer=src%2Fportfolio%2Fwhat_cms_update.md", __ASTRO_IMAGE_IMPORT_1uno4J]]);
+export default new Map([["../portfolio/images/image_new.jpg?astroContentImageFlag=&importer=src%2Fportfolio%2Ftest_item_2.md", __ASTRO_IMAGE_IMPORT_XvF3M], ["../portfolio/images/image_new.jpg?astroContentImageFlag=&importer=src%2Fportfolio%2Fwhat_cms.md", __ASTRO_IMAGE_IMPORT_Z2fuMYa], ["../portfolio/images/image_new.jpg?astroContentImageFlag=&importer=src%2Fportfolio%2Ftest_item.md", __ASTRO_IMAGE_IMPORT_ZA5H9G], ["../portfolio/images/image_new.jpg?astroContentImageFlag=&importer=src%2Fportfolio%2Fwhat_cms_update.md", __ASTRO_IMAGE_IMPORT_1uno4J]]);
 		
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,26 @@
 import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
+// import vue from "@astrojs/vue"
 
 // import mdx from "@astrojs/mdx";
 
 import sitemap from "@astrojs/sitemap";
-
 import sentry from "@sentry/astro";
 import spotlightjs from "@spotlightjs/astro";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
+    // vue(),
     react(),
     // mdx(),
     sitemap(),
     sentry(),
     spotlightjs(),
   ],
+
+  adapter: cloudflare(),
 });
